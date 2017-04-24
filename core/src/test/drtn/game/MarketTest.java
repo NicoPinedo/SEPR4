96b795a761e8e343cdb10c14ffb87928fa767ea6
jm179796 <jm1797@york.ac.uk>
2017-04-24 11:51:37 +0100

    Removed some market tests

--- core/src/test/drtn/game/MarketTest.java
+++ core/src/test/drtn/game/MarketTest.java
@@ -1,7 +1,6 @@
 package drtn.game;
 
 import com.badlogic.gdx.Game;
-import drtn.game.GameEngine;
 import drtn.game.entity.Market;
 import drtn.game.entity.Player;
 import drtn.game.enums.ResourceType;
@@ -31,7 +30,7 @@ public class MarketTest extends TesterFile{
 
         testGameEngine = new GameEngine(testGame,testScreen);
         testGameEngine.initialisePlayers(0, 2);
-        testMarket = new Market(testGame,testGameEngine);
+        testMarket = new Market();
     }
 
     /**
@@ -97,6 +96,7 @@ public class MarketTest extends TesterFile{
 
         //Food
         TestPlayer.setResource(ResourceType.FOOD, 10);
+
         TestPlayer.setResource(ResourceType.MONEY, 100);
         testMarket.setFoodSellPrice(10);
         testMarket.setFoodBuyPrice(10);
@@ -107,7 +107,7 @@ public class MarketTest extends TesterFile{
         } catch (Exception e) {
             fail("Expected to pass");
         }
-
+        
         int TestFoodCount = 20;
         assertEquals(TestFoodCount, TestPlayer.getResource(ResourceType.FOOD));
         TestMoney = 0;
@@ -132,130 +132,6 @@ public class MarketTest extends TesterFile{
 
     }
 
-    /**
-     * Tests Invalid buy conditions for all resources.
-     * <p>
-     *     Market resources are set to 10, and the prices are set to be valid for this test.
-     *     Initial values of OreCount, OreSellPrice, OreBuyPrice are set to 10 and Money is set to 100.
-     *     Initial values of FoodCount, FoodSellPrice, FoodBuyPrice are set to 10 and Money is set to 100.
-     *     Initial values of FoodCount, FoodSellPrice, FoodBuyPrice are set to 10 and Money is set to 100.
-     *
-     *     However the player attempts to buy 100 of the resource, triggering the exception
-     * </p>
-     * @throws Exception Thrown when an invalid transaction is attempted.
-     */
-    @Test
-    public void testBuyExceptions() throws Exception {
-
-        //Energy
-        TestPlayer.setResource(ResourceType.ENERGY, 10);
-        TestPlayer.setResource(ResourceType.MONEY, 100);
-        testMarket.setEnergySellPrice(10);
-        testMarket.setEnergyBuyPrice(10);
-        testMarket.setEnergyStock(100);
-
-        try {
-            testMarket.buy(ResourceType.ENERGY, 100, TestPlayer);
-            fail("Expected an exception to be thrown");
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Insufficient money");
-        }
-
-        int TestEnergyCount = 10;
-        assertEquals(TestEnergyCount, TestPlayer.getResource(ResourceType.ENERGY));
-        int TestMoney = 100;
-        assertEquals(TestMoney, TestPlayer.getResource(ResourceType.MONEY));
-        int TestSellPrice = 10;
-        assertEquals(TestSellPrice, testMarket.getEnergySellPrice());
-        int TestBuyPrice = 10;
-        assertEquals(TestBuyPrice, testMarket.getEnergyBuyPrice());
-        int TestEnergyStock = 100;
-        assertEquals(TestEnergyStock, testMarket.getEnergyStock());
-
-        //Ore
-        TestPlayer.setResource(ResourceType.ORE, 10);
-        TestPlayer.setResource(ResourceType.MONEY, 100);
-        testMarket.setOreSellPrice(10);
-        testMarket.setOreBuyPrice(10);
-        testMarket.setOreStock(100);
-
-        try {
-            testMarket.buy(ResourceType.ORE, 100, TestPlayer);
-            fail("Expected an exception to be thrown");
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Insufficient money");
-        }
-
-        int TestOreCount = 10;
-        assertEquals(TestOreCount, TestPlayer.getResource(ResourceType.ORE));
-        TestMoney = 100;
-        assertEquals(TestMoney, TestPlayer.getResource(ResourceType.MONEY));
-        TestSellPrice = 10;
-        assertEquals(TestSellPrice, testMarket.getOreSellPrice());
-        TestBuyPrice = 10;
-        assertEquals(TestBuyPrice, testMarket.getOreBuyPrice());
-        int TestOreStock = 100;
-        assertEquals(TestOreStock, testMarket.getOreStock());
-
-        //Food
-        TestPlayer.setResource(ResourceType.FOOD, 10);
-        TestPlayer.setResource(ResourceType.MONEY, 100);
-        testMarket.setFoodSellPrice(10);
-        testMarket.setFoodBuyPrice(10);
-        testMarket.setFoodStock(100);
-
-        try {
-            testMarket.buy(ResourceType.FOOD, 100, TestPlayer);
-            fail("Expected an exception to be thrown");
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Insufficient money");
-        }
-
-        int TestFoodCount = 10;
-        assertEquals(TestFoodCount, TestPlayer.getResource(ResourceType.FOOD));
-        TestMoney = 100;
-        assertEquals(TestMoney, TestPlayer.getResource(ResourceType.MONEY));
-        TestSellPrice = 10;
-        assertEquals(TestSellPrice, testMarket.getFoodSellPrice());
-        TestBuyPrice = 10;
-        assertEquals(TestBuyPrice, testMarket.getFoodBuyPrice());
-        int TestFoodStock = 100;
-        assertEquals(TestFoodStock, testMarket.getFoodStock());
-
-        //Roboticon
-        TestPlayer.setResource(ResourceType.MONEY, 1);
-        testMarket.setRoboticonBuyPrice(10);
-        testMarket.setRoboticonStock(10);
-
-        try {
-            testMarket.buy(ResourceType.ROBOTICON, 10, TestPlayer);
-            fail("Expected an exception to be thrown");
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Insufficient money");
-        }
-
-        TestPlayer.setResource(ResourceType.MONEY, 100);
-        testMarket.setRoboticonBuyPrice(10);
-        testMarket.setRoboticonStock(0);
-
-        try {
-            testMarket.buy(ResourceType.ROBOTICON, 10, TestPlayer);
-            fail("Expected an exception to be thrown");
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "No available Roboticons");
-        }
-    }
-
-    /**
-     * Tests Valid sell conditions for all resources.
-     * <p>
-     *     Market resources are set to 10, and the prices are set to be valid for this test.
-     *     Initial values of OreCount, OreSellPrice, OreBuyPrice are set to 10 and Money is set to 10.
-     *     Initial values of FoodCount, FoodSellPrice, FoodBuyPrice are set to 10 and Money is set to 10.
-     *     Initial values of FoodCount, FoodSellPrice, FoodBuyPrice are set to 10 and Money is set to 10.
-     * </p>
-     * @throws Exception Thrown when an invalid transaction is attempted.
-     */
     @Test
     public void testSell() throws Exception{
         //ore
@@ -309,50 +185,6 @@ public class MarketTest extends TesterFile{
      * </p>
      * @throws Exception Thrown when an invalid transaction is attempted.
      */
-    @Test
-    public void testSellExceptions() throws Exception{
-        //ore
-        TestPlayer.setResource(ResourceType.FOOD, 10);
-        TestPlayer.setResource(ResourceType.MONEY, 10);
-        testMarket.setOreSellPrice(10);
-        testMarket.setOreBuyPrice(10);
-        testMarket.setOreStock(10);
-
-        try {
-            testMarket.sell(ResourceType.ORE, 100, TestPlayer);
-            fail("Expected an exception to be thrown");
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Insufficient resources");
-        }
-
-        //food
-        TestPlayer.setResource(ResourceType.FOOD, 10);
-        TestPlayer.setResource(ResourceType.MONEY, 10);
-        testMarket.setFoodSellPrice(10);
-        testMarket.setFoodBuyPrice(10);
-        testMarket.setFoodStock(10);
-
-        try {
-            testMarket.sell(ResourceType.FOOD, 100, TestPlayer);
-            fail("Expected an exception to be thrown");
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Insufficient resources");
-        }
-
-        //energy
-        TestPlayer.setResource(ResourceType.ENERGY, 10);
-        TestPlayer.setResource(ResourceType.MONEY, 10);
-        testMarket.setEnergySellPrice(10);
-        testMarket.setEnergyBuyPrice(10);
-        testMarket.setEnergyStock(10);
-        try {
-            testMarket.sell(ResourceType.ENERGY, 100, TestPlayer);
-            fail("Expected an exception to be thrown");
-        } catch (Exception e) {
-            assertEquals(e.getMessage(), "Insufficient resources");
-        }
-
-    }
 
     @Test
     public void testGamble(){
@@ -414,13 +246,14 @@ public class MarketTest extends TesterFile{
         initialbuyprice = testMarket.getFoodBuyPrice();
         initialsellprice = testMarket.getFoodSellPrice();
 
+
         try {
             testMarket.buy(ResourceType.FOOD, 10, TestPlayer);
         } catch (Exception e) {
             fail("Expected to pass");
         }
 
-        assertTrue(initialbuyprice < testMarket.getFoodBuyPrice());
+        assertTrue(initialbuyprice > testMarket.getFoodBuyPrice());
         assertTrue(initialsellprice > testMarket.getFoodSellPrice());
     }
 
